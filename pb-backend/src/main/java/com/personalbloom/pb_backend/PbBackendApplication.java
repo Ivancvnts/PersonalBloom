0f54d32e4904f8b1ed2120ed0cef3be26904f4b8
Ivancvnts <ivancvnts@hotmail.com>
2025-03-30 12:39:18 -0600

    Dependencies Added

--- pb-backend/src/main/java/com/personalbloom/pb_backend/PbBackendApplication.java
+++ pb-backend/src/main/java/com/personalbloom/pb_backend/PbBackendApplication.java
@@ -2,11 +2,19 @@ package com.personalbloom.pb_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class PbBackendApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+		String dbUsername = dotenv.get("DB_USERNAME");
+		String dbPassword = dotenv.get("DB_PASSWORD");
+
+		System.setProperty("spring.datasource.username", dbUsername);
+		System.setProperty("spring.datasource.password", dbPassword);
 		SpringApplication.run(PbBackendApplication.class, args);
 	}
 
